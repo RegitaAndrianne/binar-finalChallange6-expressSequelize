369df00233a29203337cd04c2ce42299e80e7dd4
Regita Andrianne <Gitaandria95@gmail.com>
2022-02-17 23:58:24 +0700

    feat: add user

--- controller/userController.js
+++ controller/userController.js
@@ -16,6 +16,37 @@ class UserController {
             console.log(error)
         })
     }
+
+    static viewById(req, res){
+        const id = req.params.id
+        User.findByPk(id)
+        .then((data) => {
+            data = [data]
+            res.render("users", { data })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    static getAddForm(req, res){
+        res.render("users/add")
+    }
+
+    static addStudent(req, res){
+        let obj = {
+            username: req.body.username,
+            password: req.body.last_name,
+        }
+
+        User.create(newUser)
+        .then(() => {
+            res.redirect("/users")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 }
 
 module.exports = UserController;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,5 +4,10 @@ const usersRouter = require('express').Router()
 
 usersRouter.get("/hello", UserController.renderHello)
 usersRouter.get("/", UserController.viewAll)
+usersRouter.get("/add", UserController.getAddForm)
+usersRouter.get("/add", UserController.addUser)
+
+usersRouter.get("/:id", UserController.viewById)
+
 
 module.exports = usersRouter;
